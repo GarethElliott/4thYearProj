68758d2916948d457cf9e04fdccbb780ba2e940c
GarethElliott <X00106476@ittd.ie>
2015-11-25 16:49:30 +0000

    Changes made to background.

--- app/src/main/java/com/quickpic/gareth/quickpic/MainActivity.java
+++ app/src/main/java/com/quickpic/gareth/quickpic/MainActivity.java
@@ -172,10 +172,11 @@ public class MainActivity extends ActionBarActivity
                 Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
 
                 viewImage.setImageResource(0);
-
+                
                 Bitmap preview = convertToMutable(thumbnail);
                 Drawable preview2 = new BitmapDrawable(getResources(), preview);
                 viewImage.setBackground(preview2);
+
             }
         }
     }
